cbc4cff8fbd128055e5a014432c6b22868dfba45
khushi1404 <112682916+khushi1404@users.noreply.github.com>
2023-04-22 23:58:03 +0530

    Add files via upload

---
+++ FRESHGO/app/src/main/java/com/example/dti/freshgo/fragment/HomeFragment.kt
@@ -0,0 +1,69 @@
+package com.example.dti.freshgo.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.example.dti.freshgo.adapter.CategoryAdapter
+import com.example.dti.freshgo.adapter.ProductAdapter
+import com.example.dti.freshgo.databinding.FragmentHomeBinding
+import com.example.dti.freshgo.model.AddProductModel
+import com.example.dti.freshgo.model.CategoryModel
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+
+        getCategories()
+
+        getSliderImage()
+
+        getProducts()
+
+        return binding.root
+    }
+
+    private fun getSliderImage() {
+        Firebase.firestore.collection("slider").document("item")
+            .get().addOnSuccessListener {
+                Glide.with(requireContext()).load(it.get("img")).into(binding.sliderImage)
+            }
+    }
+
+    private fun getProducts() {
+        var list = ArrayList<AddProductModel>()
+        Firebase.firestore.collection("products")
+            .get().addOnSuccessListener {
+                list.clear()
+                for(doc in it.documents){
+                    val data = doc.toObject(AddProductModel::class.java)
+                    list.add(data!!)
+                }
+                binding.productRecycler.adapter = ProductAdapter(requireContext(),list)
+            }
+    }
+
+    private fun getCategories(){
+        var list = ArrayList<CategoryModel>()
+        Firebase.firestore.collection("categories")
+            .get().addOnSuccessListener {
+                list.clear()
+                for(doc in it.documents){
+                    val data = doc.toObject(CategoryModel::class.java)
+                    list.add(data!!)
+                }
+                binding.categoryRecycler.adapter = CategoryAdapter(requireContext(),list)
+            }
+    }
+}
